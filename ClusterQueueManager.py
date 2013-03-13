3ba852b946abe520fede165cc20ae3b93a947fc8
Michael Schilonka <michael@schilonka.de>
2013-03-13 23:52:19 +0100

    added feature to check in working clients

--- ClusterQueueManager.py
+++ ClusterQueueManager.py
@@ -2,41 +2,34 @@ from multiprocessing.managers import BaseManager
 from multiprocessing import Manager
 import Parameter
 
-import time
-
-
 
 class ClusterQueueManager(BaseManager):
-    clientsRunning = 0 
-    def __init__(self, myMode=''):
+    
+    def __init__(self):
         BaseManager.__init__(self,address=(Parameter.SERVER, Parameter.PORT), authkey=Parameter.PASSWORD)
         self._manager = Manager()
         self._coordinates = self._manager.Queue()
         self._values = self._manager.Queue()
-        ClusterQueueManager.register('getCoordinatesQueue', self.getCoordinatesQueue)
-        ClusterQueueManager.register('getValesQueue', self.getCoordinatesQueue)
-    
-        if myMode == 'Server':
-            print('Server')
-            self.register('clientStart', self.addClient)
-            self.register('clientDone', self.removeClient)
-        else:
-            self.register('clientStart')
-            self.register('clientDone')
-            
-        
-    
-    def getCoordinatesQueue(self):
+        self._clientCounter = self._manager.Value('i', 0)
+        self.register('clientStart', self.addClient)
+        self.register('clientDone', self.removeClient)
+        self.register('getCoordinatesQueue', self.getCoordinates)
+        self.register('getValuesQueue', self.getCoordinates)
+
+    def getCoordinates(self):
         return self._coordinates
     
     def getValuesQueue(self):
         return self._values
-
- 
+    
     def addClient(self):
-        print('added client')
-        self.clientsRunning += 1
+        self._clientCounter.set(self._clientCounter.get() + 1)
     
     def removeClient(self):
-        self.clientsRunning -= 1
+        self._clientCounter.set(self._clientCounter.get() + 1)
+        
+    def getRunningClients(self):
+        return self._clientCounter.get()
+        
+
     
