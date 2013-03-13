d02490442ceae69e30309837505305e884b90f08
Michael Schilonka <michael@schilonka.de>
2013-03-13 20:33:19 +0100

    added ClusterQueueManager features

--- ClusterQueueManager.py
+++ ClusterQueueManager.py
@@ -1,20 +1,20 @@
 from multiprocessing.managers import BaseManager
 from multiprocessing import Manager
-
+import Parameter
 
 class ClusterQueueManager(BaseManager):
-    
-    
+      
     def __init__(self, myMode):
+        BaseManager.__init__(self,address=(Parameter.SERVER, Parameter.PORT), authkey=Parameter.PASSWORD)
         self.clientsRunning = 0
         self._manager = Manager()
-        self._coordinates = Manager.Queue()
-        self._values = Manager.Queue()
+        self._coordinates = self._manager.Queue()
+        self._values = self._manager.Queue()
         self.register('getCoordinatesQueue', self.getCoordinatesQueue)
         self.register('getValesQueue', self.getCoordinatesQueue)
     
         if myMode == 'Server':
-            self.register('clientRun', self.selfe.addClient)
+            self.register('clientRun', self.addClient)
             self.register('clientDone', self.removeClient)
         else:
             self.register('clientRun')
