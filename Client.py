7f63130ec5b1ecbedd77d20f28e7abbc87ac3bce
Robert Stein <roddirockz@googlemail.com>
2013-03-13 18:42:47 +0100

    added client behaviour

--- Client.py
+++ Client.py
@@ -10,4 +10,8 @@ class ClusterClient():
     def __connectToServer():
         manager = ClusterQueueManager(address=(Parameter.SERVER, Parameter.PORT),\
                                    authkey=Parameter.PASSWORD)
+        
+        manager.register('get_queue')
         manager.connect()
+        
+        return manager.get_queue(Parameter.DEFLECTIONS)
