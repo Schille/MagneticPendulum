907d4ea0c8f5f9a07f6b2ac96c05a67b7cabc208
Robert Stein <roddirockz@googlemail.com>
2013-03-14 10:44:01 +0100

    ?

--- ClusterQueueManager.py
+++ ClusterQueueManager.py
@@ -2,28 +2,23 @@ from multiprocessing.managers import BaseManager
 from multiprocessing import Manager
 import Parameter
 
-import time
-
-
-
 class ClusterQueueManager(BaseManager):
-    clientsRunning = 0 
-    def __init__(self, myMode=''):
+      
+    def __init__(self, myMode):
         BaseManager.__init__(self,address=(Parameter.SERVER, Parameter.PORT), authkey=Parameter.PASSWORD)
+        self.clientsRunning = 0
         self._manager = Manager()
         self._coordinates = self._manager.Queue()
         self._values = self._manager.Queue()
-        ClusterQueueManager.register('getCoordinatesQueue', self.getCoordinatesQueue)
-        ClusterQueueManager.register('getValesQueue', self.getCoordinatesQueue)
+        self.register('getCoordinatesQueue', self.getCoordinatesQueue)
+        self.register('getValesQueue', self.getCoordinatesQueue)
     
         if myMode == 'Server':
-            print('Server')
-            self.register('clientStart', self.addClient)
+            self.register('clientRun', self.addClient)
             self.register('clientDone', self.removeClient)
         else:
-            self.register('clientStart')
+            self.register('clientRun')
             self.register('clientDone')
-            
         
     
     def getCoordinatesQueue(self):
@@ -31,12 +26,11 @@ class ClusterQueueManager(BaseManager):
     
     def getValuesQueue(self):
         return self._values
-
- 
+    
     def addClient(self):
-        print('added client')
         self.clientsRunning += 1
     
     def removeClient(self):
         self.clientsRunning -= 1
+    
     
