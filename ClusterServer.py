137615ecd27e85e631fad92ad73f5005ed95c8aa
Robert Stein <roddirockz@googlemail.com>
2013-03-14 11:17:06 +0100

    refreshed server

--- ClusterServer.py
+++ ClusterServer.py
@@ -1,25 +1,28 @@
-'''
-Created on 13.03.2013
-
-@author: RStein
-'''
 from multiprocessing.managers import BaseManager
 from ClusterQueueManager import ClusterQueueManager
 import Parameter
+import time
+import Image
+import sys
 
 
-class ServerManager(BaseManager):
-    def __setUp_Server(self):
-        manager = ClusterQueueManager(address=(Parameter.SERVER, Parameter.PORT),\
+        
+def startServer():
+    import Simulation
+    manager = ClusterQueueManager(address=(Parameter.SERVER, Parameter.PORT),\
                                    authkey=Parameter.PASSWORD)
-        
-        server = manager.get_server()
-        manager.start()
-        
-        
-    def __init__(self):
-        self.clients = 0
-        self.__setUp_Server()
-        
-        
-        
+    manager.start()
+    data = []
+    coordinates = manager.getCoordinates()
+    im= Image.new('RGB', (Parameter.RESOLUTION, Parameter.RESOLUTION))
+    pixel = [] + [0]*(Parameter.RESOLUTION**2)
+    Simulation.createAllCoordinates(coordinates, data)
+    values = manager.getValuesQueue() 
+    start = time.time()
+    while not coordinates.empty():
+        Simulation.drawImage(im, data, pixel, values)
+        time.sleep(Parameter.REPAINT)
+    Simulation.drawImage(im, data, pixel, values)
+    print('Image succeeded. Time consumed: {0:.2f}s'.format((time.time() - start)))
+    print('Exiting...')
+    sys.exit(0)
